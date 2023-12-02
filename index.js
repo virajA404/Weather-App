dd2cea3143fb3292b6073ea6fb8fb20bae935645
Viraj Abeywardana <abeywardana21@gmail.com>
2023-12-02 23:23:31 +0530

    changes

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
