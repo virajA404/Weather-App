df681e9e294a754b628706a11390e5612d135d62
Viraj Abeywardana <abeywardana21@gmail.com>
2023-12-03 23:39:27 +0530

    add static files

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
