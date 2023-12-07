a010788100aad381be46c5b3b2dceb5bdbf8c0bc
Viraj Abeywardana <abeywardana21@gmail.com>
2023-12-07 12:47:34 +0530

    adding comments

--- index.js
+++ index.js
@@ -13,15 +13,18 @@ let place =""
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+//Submitting the location and getting the latitude and longitude of the location. They are required to get the weather information 
 app.post('/submit', async(req, res) => {
     const location = req.body.location;
     // console.log(location);
     try{
         const locationResult = await axios.get(WEATHER_API_URL+"/geo/1.0/direct?q=" + location + "&limit=1&appid=" + API_KEY);
-        console.log(locationResult.data[0].lat);
-        console.log(locationResult.data);
+        // console.log(locationResult.data[0].lat);
+        // console.log(locationResult.data);
         console.log("place: " + locationResult.data[0].name);
         res.render('index.ejs', {latitude:locationResult.data[0].lat, longitude: locationResult.data[0].lon });
+        //assigning latitude, longitude and location to the declared variables, Since we need to use them in the get-weather method
         lat = locationResult.data[0].lat;
         lon = locationResult.data[0].lon;
         place = locationResult.data[0].name;
@@ -31,11 +34,14 @@ app.post('/submit', async(req, res) => {
     }
 })
 
+//getting the weather information of the location by giving lat and lon
 app.post('/get-weather', async(req,res) => {
     try{
-        console.log("param " + lat,lon);
+        // console.log("param " + lat,lon);
         const weather = await axios.get(WEATHER_API_URL + `/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily,minutely&units=metric&appid=`+ API_KEY);
         // console.log(weather.data);
+
+        //sending the location and weather data to ejs 
         res.render('index.ejs', {weather: weather.data.current, location:place})
     }catch (error) {
         console.log("hello")
