e570282efc060bb777c07823eaa66c11b75f494c
Viraj Abeywardana <abeywardana21@gmail.com>
2023-12-07 12:42:47 +0530

    get the location to a variable

--- index.js
+++ index.js
@@ -7,20 +7,24 @@ const port = 3000;
 const WEATHER_API_URL = "https://api.openweathermap.org";
 const API_KEY = "1566507cea6c12b3ca22edbc2bf4bd1a";
 let lat=""
-let lon=""
+let lon="" 
+let place =""
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/submit', async(req, res) => {
     const location = req.body.location;
-    console.log(location);
+    // console.log(location);
     try{
         const locationResult = await axios.get(WEATHER_API_URL+"/geo/1.0/direct?q=" + location + "&limit=1&appid=" + API_KEY);
         console.log(locationResult.data[0].lat);
+        console.log(locationResult.data);
+        console.log("place: " + locationResult.data[0].name);
         res.render('index.ejs', {latitude:locationResult.data[0].lat, longitude: locationResult.data[0].lon });
         lat = locationResult.data[0].lat;
         lon = locationResult.data[0].lon;
+        place = locationResult.data[0].name;
     }catch (error) {
         console.log("hi")
         res.status(400).send(error.message);
@@ -31,8 +35,8 @@ app.post('/get-weather', async(req,res) => {
     try{
         console.log("param " + lat,lon);
         const weather = await axios.get(WEATHER_API_URL + `/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily,minutely&units=metric&appid=`+ API_KEY);
-        console.log(weather.data);
-        res.render('index.ejs', {weather: weather.data.current})
+        // console.log(weather.data);
+        res.render('index.ejs', {weather: weather.data.current, location:place})
     }catch (error) {
         console.log("hello")
         res.status(400).send(error.message);
